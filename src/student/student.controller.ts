03b523794aa080f49f08a8696848be9b0f74f712
Miyekahh <miecacarlit@gmail.com>
2025-01-11 23:48:47 +0800

    done with update student

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common';
+import { StudentService } from './student.service';
+import { Student } from './entities/student.entity';
 
-@Controller('student')
-export class StudentController {}
+@Controller('students')
+export class StudentController {
+    constructor(private readonly studentService: StudentService) { }
+
+    // Endpoint to create a student
+    @Post()
+    create(@Body() studentData: Partial<Student>): Promise<Student> {
+        return this.studentService.create(studentData);
+    }
+
+    // Endpoint to fetch all students
+    @Get()
+    findAll(): Promise<Student[]> {
+        return this.studentService.findAll();
+    }
+    @Get(':id')
+    async findOne(@Param('id') id: number) {
+    return this.studentService.findOne(id);
+}
+@Patch(':id')
+update(@Param('id') id: number, @Body() updateData: Partial<Student>) {
+    return this.studentService.update(id, updateData);
+}
+
+
+
+}
