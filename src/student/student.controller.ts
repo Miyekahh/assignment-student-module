23dec93067f2da0bc2a8f64ee8aeab6c1165ae65
Miyekahh <miecacarlit@gmail.com>
2025-01-12 22:45:00 +0800

    done

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,10 +1,6 @@
 import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common';
 import { StudentService } from './student.service';
 import { Student } from './entities/student.entities';
-import { Controller, Get, Post, Body, Param, Patch, Delete } from '@nestjs/common';
-import { StudentService } from './student.service';
-import { Student } from './entities/student.entities';
-
 @Controller('students')
 export class StudentController {
     constructor(private readonly studentService: StudentService) { }
